627efac67ba65e535d8f8119bc7333fcd6252186
Maciej Kosmela <alternatywnykosmos@gmail.com>
2021-08-05 14:09:12 +0200

    I try make anchor scroll but nothing not work at the time

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+// import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
+export class NavComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  // constructor(private viewportScroller: ViewportScroller) {}
+  // public onClick(elementId: string): void { 
+  //   this.viewportScroller.scrollToAnchor(elementId);
+  // }
+  public scroll($element): void {
+    // 
+     
+      console.log($element);
+      $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    
+  };
 }
