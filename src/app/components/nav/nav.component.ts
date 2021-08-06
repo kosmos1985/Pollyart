24935537455b03cb7b8520bc3cfcf830190db6e3
Maciej Kosmela <alternatywnykosmos@gmail.com>
2021-08-06 14:36:54 +0200

    work at responsive

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -13,19 +13,19 @@ export class NavComponent  {
   //   this.viewportScroller.scrollToAnchor(elementId);
   // }
   public scrollToAbout(): void {
-    window.scrollTo({ left: 0, top: 700, behavior: 'smooth' });
+    window.scrollTo({ left: 0, top: 500, behavior: 'smooth' });
   };
   public scrollToPractis(): void {
-    window.scrollTo({ left: 0, top: 1300, behavior: 'smooth' });
+    window.scrollTo({ left: 0, top: 1100, behavior: 'smooth' });
   };
   public scrollToLawyers(): void {
-    window.scrollTo({ left: 0, top: 1800, behavior: 'smooth' });
+    window.scrollTo({ left: 0, top: 1600, behavior: 'smooth' });
   };
   public scrollToPublications(): void {
-    window.scrollTo({ left: 0, top: 3000, behavior: 'smooth' });
+    window.scrollTo({ left: 0, top: 2800, behavior: 'smooth' });
   };
   public scrollToFaq(): void {
-    window.scrollTo({ left: 0, top: 3700, behavior: 'smooth' });
+    window.scrollTo({ left: 0, top: 3550, behavior: 'smooth' });
   };
   public scrollToContact(): void {
     window.scrollTo({ left: 0, top: 4400, behavior: 'smooth' });
